af155aa92ae985dae713f4a988f30aa725a51434
zenonet <78077158+zenonet@users.noreply.github.com>
2024-03-17 20:19:51 +0100

    Added a helper method for Lesson that returns whether the type indicates the lesson is taking place

--- app/src/main/java/de/zenonet/stundenplan/Lesson.java
+++ app/src/main/java/de/zenonet/stundenplan/Lesson.java
@@ -10,4 +10,8 @@ public class Lesson {
     public LessonType Type = LessonType.Regular;
     public transient LocalTime StartTime;
     public transient LocalTime EndTime;
+
+    public boolean isTakingPlace(){
+        return Type == LessonType.Cancelled || Type == LessonType.Absent || Type == LessonType.Holiday;
+    }
 }
