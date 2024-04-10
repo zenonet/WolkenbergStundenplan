2b4ff10b844df4bc73d1394c5fe60e5f17f0ab32
zenonet <78077158+zenonet@users.noreply.github.com>
2024-04-10 14:36:40 +0200

    Finally made the LoginActivity work

--- app/src/main/java/de/zenonet/stundenplan/activities/LoginActivity.java
+++ app/src/main/java/de/zenonet/stundenplan/activities/LoginActivity.java
@@ -3,7 +3,7 @@ package de.zenonet.stundenplan.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
+import android.view.KeyEvent;
 import android.webkit.*;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_token_extractor);
+        setContentView(R.layout.activity_login_view);
 
         // Clear all the Application Cache, Web SQL Database and the HTML5 Web Storage
         WebStorage.getInstance().deleteAllData();
@@ -33,19 +33,38 @@ public class LoginActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                if (url.startsWith("https://www.wolkenberg-gymnasium.de/wolkenberg-app/stundenplan-web-app/?code=")) {
+            public void onLoadResource(WebView view, String url) {
+                checkForCodeUrl(view, url);
 
-                    String refreshToken = extractRefreshTokenFromUrl(url);
-                    saveRefreshToken(refreshToken);
 
-                    // Log.i("TokenExtractor", "onPageStarted: Found the code (I think): " + refreshToken);
+            }
 
-                    Toast.makeText(LoginActivity.this, "Refresh token found!",  Toast.LENGTH_SHORT).show();
+            public boolean shouldOverrideUrlLoading (WebView view, String url) {
+                return checkForCodeUrl(view, url);
+            }
+            public boolean shouldOverrideUrlLoading (WebView view, WebResourceRequest request) {
+                return checkForCodeUrl(view, request.getUrl().toString());
+            }
 
-                    Intent intent = new Intent(LoginActivity.this, TimeTableViewActivity.class);
-                    startActivity(intent);
-                }
+            public boolean shouldOverrideKeyEvent (WebView view, KeyEvent event) {
+                return true;
+            }
+
+
+            private boolean checkForCodeUrl(WebView view, String url) {
+                if (!url.startsWith("https://www.wolkenberg-gymnasium.de/wolkenberg-app/stundenplan-web-app/?code="))
+                    return false;
+
+                String oAuthCode = extractRefreshTokenFromUrl(url);
+
+                // Log.i("TokenExtractor", "onPageStarted: Found the code (I think): " + refreshToken);
+
+                Toast.makeText(LoginActivity.this, "Refresh token found!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(LoginActivity.this, TimeTableViewActivity.class);
+                intent.putExtra("code", oAuthCode);
+                startActivity(intent);
+                return true;
             }
         });
         webView.clearCache(true);
@@ -63,11 +82,5 @@ public class LoginActivity extends AppCompatActivity {
         if (!matcher.matches()) return null;
 
         return matcher.group(1);
-    }
-    private void saveRefreshToken(String token){
-        SharedPreferences sharedPreferences = getSharedPreferences("de.zenonet.stundenplan", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("refreshToken", token);
-        editor.apply();
     }
 }
