8d7a2a43ac8ac23b4c6681ddc592e47d970842db
zenonet <78077158+zenonet@users.noreply.github.com>
2024-04-14 12:30:44 +0200

    Disabled minify for release builds for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false // TODO Enable minify for publishing
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
         }
@@ -38,6 +38,9 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.activity:activity:1.8.2")
+    implementation("androidx.preference:preference-ktx:1.2.0")
+   // implementation ("com.android.support:appcompat-v7:23.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
