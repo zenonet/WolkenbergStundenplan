80101b5f6d218cfef45f27d8bf222fa62d77e490
zenonet <78077158+zenonet@users.noreply.github.com>
2024-05-08 18:38:12 +0200

    Improved Quote downloading by using etags for cache management

--- Common/src/main/java/de/zenonet/stundenplan/common/quoteOfTheDay/QuoteProvider.java
+++ Common/src/main/java/de/zenonet/stundenplan/common/quoteOfTheDay/QuoteProvider.java
@@ -1,17 +1,19 @@
 package de.zenonet.stundenplan.common.quoteOfTheDay;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.time.LocalDate;
 
 
@@ -34,7 +36,7 @@ public class QuoteProvider {
 
     private boolean loadQuotesFromApi() {
         try {
-            HttpURLConnection httpCon = (HttpURLConnection) new URL("http://api.zenonet.de/stundenplan/quotes.json").openConnection();
+            HttpURLConnection httpCon = (HttpURLConnection) new URL("https://zenonet.de/stundenplan/quotes.json").openConnection();
             httpCon.connect();
 
             if (httpCon.getResponseCode() != 200)
@@ -48,6 +50,13 @@ public class QuoteProvider {
                 stream.write(content.getBytes(StandardCharsets.UTF_8));
             }
 
+
+            // Save etag of the file
+            File etagFile = new File(Utils.CachePath, "/etag.txt");
+            try (FileOutputStream stream = new FileOutputStream(etagFile)) {
+                stream.write(httpCon.getHeaderField("Etag").getBytes(StandardCharsets.UTF_8));
+            }
+
             return true;
 
         } catch (IOException e) {
@@ -56,15 +65,13 @@ public class QuoteProvider {
     }
 
     public Quote getQuoteOfTheDay() {
+        maybeCheckForNewQuotes();
         try {
             File file = new File(Utils.CachePath, "quotes.json");
+            if(!file.exists())
+                loadQuotesFromApi();
 
-            int length = (int) file.length();
-            byte[] bytes = new byte[length];
-            try (FileInputStream in = new FileInputStream(file)) {
-                in.read(bytes);
-            }
-            String contents = new String(bytes);
+            String contents = Utils.readAllText(file);
             JSONObject root = new JSONObject(contents);
 
             int index = (int) (LocalDate.now().toEpochDay() % root.getInt("count"));
@@ -72,5 +79,39 @@ public class QuoteProvider {
         } catch (IOException | JSONException ignored) {
             return null;
         }
+    }
+
+    public void maybeCheckForNewQuotes() {
+        File cacheFile = new File(Utils.CachePath, "/quotes.json");
+        long distance = (Instant.now().getEpochSecond() - cacheFile.lastModified() / 1000);
+        // If the cache file was last changed more than one week ago
+        //if (distance <= 60 * 60 * 24) return;
+
+        try {
+
+            File etagFile = new File(Utils.CachePath, "/etag.txt");
+
+            if(!etagFile.exists()){
+                loadQuotesFromApi();
+                return;
+            }
+
+            String localEtag = Utils.readAllText(etagFile);
+
+            HttpURLConnection httpCon;
+            httpCon = (HttpURLConnection) new URL("https://zenonet.de/stundenplan/quotes.json").openConnection();
+            httpCon.setRequestMethod("HEAD");
+            httpCon.connect();
+
+            String etag = httpCon.getHeaderField("Etag");
+
+            if(!localEtag.equals(etag)){
+                loadQuotesFromApi();
+            }
+
+        } catch (IOException e) {
+            Log.e(Utils.LOG_TAG, e.toString());
+        }
+
     }
 }
