37657b58a46d3cfa66381810bcf8dc6b2beefe93
zenonet <78077158+zenonet@users.noreply.github.com>
2024-05-26 12:39:48 +0200

    Updated some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,14 +55,14 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.activity:activity:1.8.2")
-    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.activity:activity:1.9.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
