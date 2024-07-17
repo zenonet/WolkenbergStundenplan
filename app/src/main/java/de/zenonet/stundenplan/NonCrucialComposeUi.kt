71c730ad327fe5528ad60b695f5c3ade5754497f
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-17 14:12:28 +0200

    Increased versions

--- app/src/main/java/de/zenonet/stundenplan/NonCrucialComposeUi.kt
+++ app/src/main/java/de/zenonet/stundenplan/NonCrucialComposeUi.kt
@@ -1,7 +1,6 @@
 package de.zenonet.stundenplan
 
 import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.font.FontStyle
@@ -21,7 +19,6 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.LiveData
 import de.zenonet.stundenplan.common.Utils
 import de.zenonet.stundenplan.common.quoteOfTheDay.Quote
 
