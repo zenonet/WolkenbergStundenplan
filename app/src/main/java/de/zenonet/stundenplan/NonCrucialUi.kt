zenonet/WolkenbergStundenplan

commit 59d9ce474ec9d431f32b0496359b1633b00610a4

Moved recomputation of lesson progress to viewmodelScope coroutine. Also other improvements to NonCrucialUi

app/src/main/java/de/zenonet/stundenplan/NonCrucialUi.kt
@@ -92,12 +92,6 @@ fun CurrentLessonInfo(vm: NonCrucialViewModel, modifier: Modifier = Modifier) {
     // Don't show this on week-ends
     if (dayOfWeek > 4 || dayOfWeek < 0) return
 
-    LaunchedEffect(null) {
-        while (true) {
-            vm.generateCurrentLessonInfoData()
-            delay(1000 * 60)
-        }
-    }
 
     if (vm.currentPeriod == -1 || vm.currentTime.isBefore(LocalTime.of(8, 0))) return
 
@@ -114,31 +108,29 @@ fun CurrentLessonInfo(vm: NonCrucialViewModel, modifier: Modifier = Modifier) {
             Text("Von ${vm.startTime} bis ${vm.endTime} ${if (vm.lessonProgress > 0) " (${vm.lessonProgress}%)" else ""}")
             Spacer(Modifier.height(10.dp))
 
-            if(vm.isBreak){
-                Text("Danach:")
-            }
+            if (vm.isBreak)
+                Text("Nach der Pause:", fontWeight = FontWeight.Bold)
 
-            val day: Array<Lesson>? = timeTable?.Lessons?.get(dayOfWeek)
-            val lesson: Lesson? = if (day != null && day.size > vm.currentPeriod) day[vm.currentPeriod] else null
+            val day: Array<Lesson?>? = timeTable?.Lessons?.get(dayOfWeek)
+            val lesson: Lesson? =
+                if (day != null && day.size > vm.currentPeriod) day[vm.currentPeriod] else null
 
-
+            // if lesson is null, this means, it's a regular free period
             if (lesson != null) {
                 LessonInfoSentence(lesson)
             }
-            
-            if (day != null) {
+            else if (day != null) {
 
                 var nextPeriod = vm.currentPeriod + 1
-                while (nextPeriod < day.size && (day[nextPeriod] == null || !day[nextPeriod].isTakingPlace)) nextPeriod++
+                while (nextPeriod < day.size && !Lesson.doesTakePlace(day[nextPeriod])) nextPeriod++
 
                 if (nextPeriod < day.size) {
+                    // The loop can only stop if nextPeriod reached the end (in which case we wouldn't be in this if) or if day[nextPeriod] is not null, so nextLesson will never be null
+                    val nextLesson: Lesson = day[nextPeriod]!!
+                    Text("Freistunde")
+                    Text("Danach:", fontWeight = FontWeight.Bold)
+                    Text("${nextLesson.Subject} in ${nextLesson.Room} (beginnt um ${nextLesson.StartTime})")
 
-                    val nextLesson = day[nextPeriod]
-                    if (lesson == null || !lesson.isTakingPlace) { // Freistunden
-                        Text("Freistunde", fontWeight = FontWeight.Bold)
-                        Text("Danach:")
-                        Text("${nextLesson.Subject} in ${nextLesson.Room} (beginnt um ${nextLesson.StartTime})")
-                    }
                 }
             }
 

app/src/main/java/de/zenonet/stundenplan/NonCrucialViewModel.kt
@@ -16,6 +16,7 @@ import de.zenonet.stundenplan.common.quoteOfTheDay.QuoteProvider
 import de.zenonet.stundenplan.common.timetableManagement.TimeTable
 import de.zenonet.stundenplan.common.timetableManagement.TimeTableManager
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -45,6 +46,14 @@ class NonCrucialViewModel(
 
 
     init {
+        // Update progress regularly (this is implemented here because it's the right place for it according to this: https://developer.android.com/topic/libraries/architecture/coroutines#viewmodelscope)
+        viewModelScope.launch {
+            while (true) {
+                generateCurrentLessonInfoData()
+                delay(1000 * 27) // Updating every 27 seconds means that every percent of a 45 minute lesson will be shown (1%*45min = 45min/100 = 2700s/100 = 27s)
+            }
+        }
+
         if (quote != null) {
             _quoteOfTheDay.value = quote
         }
@@ -126,23 +135,18 @@ class NonCrucialViewModel(
         return stairCases
     }
 
-    fun loadQuoteOfTheDay() {
+    suspend fun loadQuoteOfTheDay() {
         if (quote != null) return
-
-        viewModelScope.launch {
-
-            val q = withContext(Dispatchers.IO) {
-                try {
-                    quoteProvider.getQuoteOfTheDay()
-                } catch (_: Exception) {
-                    null
-                }
+        val q = withContext(Dispatchers.IO) {
+            try {
+                quoteProvider.getQuoteOfTheDay()
+            } catch (_: Exception) {
+                null
             }
-            if (q != null) {
-                _quoteOfTheDay.value = q
-                Log.i(LogTags.Debug, "Assigned quote to state")
-            }
-
+        }
+        if (q != null) {
+            _quoteOfTheDay.value = q
+            Log.i(LogTags.Debug, "Assigned quote to state")
         }
     }
 
@@ -165,10 +169,10 @@ class NonCrucialViewModel(
         if (startTime != null && endTime != null) {
             isBreak = startTime!!.isAfter(currentTime)
 
-            if(isBreak && currentPeriod > 0){
+            if (isBreak && currentPeriod > 0) {
                 endTime = startTime
 
-                val pairOfLessonBefore = Utils.getStartAndEndTimeOfPeriod(currentPeriod-1)
+                val pairOfLessonBefore = Utils.getStartAndEndTimeOfPeriod(currentPeriod - 1)
                 startTime = pairOfLessonBefore.second
             }
 

Common/src/main/java/de/zenonet/stundenplan/common/timetableManagement/Lesson.java
@@ -36,4 +36,8 @@ public final class Lesson {
         Lesson other = (Lesson) obj;
         return Objects.equals(other.Room, Room) && Objects.equals(other.SubjectShortName, SubjectShortName) && Objects.equals(other.Teacher, Teacher) && other.Type == Type;
     }
+
+    public static boolean doesTakePlace(Lesson lesson){
+        return lesson != null && lesson.isTakingPlace();
+    }
 }

commit 332ff9e09b10b349637b770246fe2cd57e630e77

Fixed assignment of MutableStates before they're properly initialized in NonCrucialViewModel (fixing a crash introduced in 59d9ce47)

app/src/main/java/de/zenonet/stundenplan/NonCrucialUi.kt
@@ -93,6 +93,10 @@ fun CurrentLessonInfo(vm: NonCrucialViewModel, modifier: Modifier = Modifier) {
     if (dayOfWeek > 4 || dayOfWeek < 0) return
 
 
+    LaunchedEffect(Unit) {
+        vm.startRegularDataRecalculation()
+    }
+
     if (vm.currentPeriod == -1 || vm.currentTime.isBefore(LocalTime.of(8, 0))) return
 
     val timeTable by vm.currentTimeTable.collectAsStateWithLifecycle(null)

app/src/main/java/de/zenonet/stundenplan/NonCrucialViewModel.kt
@@ -46,14 +46,6 @@ class NonCrucialViewModel(
 
 
     init {
-        // Update progress regularly (this is implemented here because it's the right place for it according to this: https://developer.android.com/topic/libraries/architecture/coroutines#viewmodelscope)
-        viewModelScope.launch {
-            while (true) {
-                generateCurrentLessonInfoData()
-                delay(1000 * 27) // Updating every 27 seconds means that every percent of a 45 minute lesson will be shown (1%*45min = 45min/100 = 2700s/100 = 27s)
-            }
-        }
-
         if (quote != null) {
             _quoteOfTheDay.value = quote
         }
@@ -149,6 +141,17 @@ class NonCrucialViewModel(
             Log.i(LogTags.Debug, "Assigned quote to state")
         }
     }
+
+    fun startRegularDataRecalculation() {
+        // Update progress regularly (this is implemented here because it's the right place for it according to this: https://developer.android.com/topic/libraries/architecture/coroutines#viewmodelscope)
+        viewModelScope.launch {
+            while (true) {
+                generateCurrentLessonInfoData()
+                delay(1000 * 27) // Updating every 27 seconds means that every percent of a 45 minute lesson will be shown (1%*45min = 45min/100 = 2700s/100 = 27s)
+            }
+        }
+    }
+
 
     var currentPeriod by mutableIntStateOf(-1)
     var startTime: LocalTime? by mutableStateOf(LocalTime.MIN)