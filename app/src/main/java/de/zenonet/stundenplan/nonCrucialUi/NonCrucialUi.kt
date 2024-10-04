c7025da270967c21ccbb0684f077d0408bb1b1eb
zenonet <78077158+zenonet@users.noreply.github.com>
2024-10-04 14:37:37 +0200

    Made NonCrucialUi widget choices permanent using zhanghai's compose preference lib

--- app/src/main/java/de/zenonet/stundenplan/nonCrucialUi/NonCrucialUi.kt
+++ app/src/main/java/de/zenonet/stundenplan/nonCrucialUi/NonCrucialUi.kt
@@ -1,33 +1,14 @@
 package de.zenonet.stundenplan.nonCrucialUi
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.text.font.FontStyle
@@ -39,7 +20,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import de.zenonet.stundenplan.common.Timing
 import de.zenonet.stundenplan.common.quoteOfTheDay.Quote
 import de.zenonet.stundenplan.common.timetableManagement.Lesson
+import de.zenonet.stundenplan.nonCrucialUi.widgets.Widget
 import de.zenonet.stundenplan.ui.theme.StundenplanTheme
+import me.zhanghai.compose.preference.ProvidePreferenceLocals
 import java.time.LocalTime
 
 fun applyUiToComposeView(view: ComposeView, viewModel: NonCrucialViewModel) {
@@ -57,19 +40,21 @@ fun Main(viewModel: NonCrucialViewModel, modifier: Modifier = Modifier) {
 
 
         Surface {
-            Column {
+            ProvidePreferenceLocals {
+                Column {
 
-                CurrentLessonInfo(viewModel)
+                    CurrentLessonInfo(viewModel)
 
-                LaunchedEffect(key1 = null) {
-                    viewModel.loadQuoteOfTheDay()
+                    LaunchedEffect(key1 = null) {
+                        viewModel.loadQuoteOfTheDay()
+                    }
+                    val quote by viewModel.quoteOfTheDay.collectAsStateWithLifecycle(null)
+                    if (quote != null && quote!!.text != null)
+                        QuoteView(quote!!)
+
+
+                    DailyStaircaseAnalysis(viewModel)
                 }
-                val quote by viewModel.quoteOfTheDay.collectAsStateWithLifecycle(null)
-                if (quote != null && quote!!.text != null)
-                    QuoteView(quote!!)
-
-
-                DailyStaircaseAnalysis(viewModel)
             }
 
         }
@@ -178,72 +163,6 @@ fun Heading(text: String) {
         fontSize = 20.sp,
         fontWeight = FontWeight.Bold
     )
-}
-
-@Composable
-fun Widget(
-    widgetKey:String,
-    modifier: Modifier = Modifier,
-    content: @Composable (ColumnScope.() -> Unit)
-) {
-
-    var showConfirmationDialog by rememberSaveable { mutableStateOf(false) }
-    var show by rememberSaveable { mutableStateOf(true) }
-    AnimatedVisibility(show) {
-        Box {
-
-
-            Card(
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(38, 36, 42),
-                ),
-                //elevation = CardDefaults.cardElevation(4.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(5.dp)
-            ) {
-
-                Column(modifier.padding(16.dp)) {
-                    content()
-                }
-            }
-
-            IconButton(
-                onClick = { showConfirmationDialog = true },
-                modifier = Modifier
-                    .padding(8.dp)
-                    .align(TopEnd)
-                    .size(36.dp)
-                    .padding(8.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Close,
-                    contentDescription = "Close",
-                )
-            }
-
-            if(showConfirmationDialog)
-            AlertDialog(
-                title = {
-                    Text("Entfernen bestätigen")
-                },
-                text = {
-                    Text("Möchtest du diese Karte wirklich dauerhaft entfernen? Du kannst dies in den Einstellungen rückgängig machen")
-                },
-                onDismissRequest = {showConfirmationDialog = false},
-                confirmButton = {
-                    Button({show = false; showConfirmationDialog = false}){
-                        Text("Ja")
-                    }
-                },
-                dismissButton = {
-                    Button({showConfirmationDialog = false}){
-                        Text("Nein")
-                    }
-                })
-        }
-
-    }
 }
 
 @Composable
---
+++ app/src/main/java/de/zenonet/stundenplan/nonCrucialUi/widgets/Widget.kt
@@ -0,0 +1,94 @@
+package de.zenonet.stundenplan.nonCrucialUi.widgets
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import me.zhanghai.compose.preference.LocalPreferenceFlow
+import me.zhanghai.compose.preference.rememberPreferenceState
+
+@Composable
+fun Widget(
+    widgetKey:String,
+    modifier: Modifier = Modifier,
+    content: @Composable (ColumnScope.() -> Unit)
+) {
+    var showConfirmationDialog by rememberSaveable { mutableStateOf(false) }
+    var show by rememberPreferenceState("nonCrucialUi.$widgetKey", true)
+    AnimatedVisibility(show) {
+        Box {
+
+
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(38, 36, 42),
+                ),
+                //elevation = CardDefaults.cardElevation(4.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp)
+            ) {
+
+                Column(modifier.padding(16.dp)) {
+                    content()
+                }
+            }
+
+            IconButton(
+                onClick = { showConfirmationDialog = true },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopEnd)
+                    .size(36.dp)
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Close",
+                )
+            }
+
+            if (showConfirmationDialog)
+                AlertDialog(
+                    title = {
+                        Text("Entfernen bestätigen")
+                    },
+                    text = {
+                        Text("Möchtest du diese Karte wirklich dauerhaft entfernen? Du kannst dies in den Einstellungen rückgängig machen")
+                    },
+                    onDismissRequest = { showConfirmationDialog = false },
+                    confirmButton = {
+                        Button({ show = false; showConfirmationDialog = false }) {
+                            Text("Ja")
+                        }
+                    },
+                    dismissButton = {
+                        Button({ showConfirmationDialog = false }) {
+                            Text("Nein")
+                        }
+                    })
+        }
+
+    }
+}
