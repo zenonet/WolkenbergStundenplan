1b5e19528547685a8d9fa553015b5fcad95b81ca
zenonet <78077158+zenonet@users.noreply.github.com>
2024-10-26 12:01:16 +0200

    Added comment about a potential faster loading login site in LoginActivity.java

--- app/src/main/java/de/zenonet/stundenplan/activities/LoginActivity.java
+++ app/src/main/java/de/zenonet/stundenplan/activities/LoginActivity.java
@@ -66,6 +66,10 @@ public class LoginActivity extends AppCompatActivity {
         settings.setJavaScriptEnabled(true);
         settings.setAllowContentAccess(true);
         webView.loadUrl("https://www.wolkenberg-gymnasium.de/wolkenberg-app/stundenplan-web-app");
+
+        // This would work as well and would be significantly faster:
+        //webView.loadUrl("https://login.microsoftonline.com/wgmail.de/oauth2/v2.0/authorize?client_id=fb82e2a9-1efd-4a8e-9ac6-92413ab4b58b&response_type=code&redirect_uri=https%3A%2F%2Fwww.wolkenberg-gymnasium.de%2Fwolkenberg-app%2Fstundenplan-web-app%2F&response_mode=query&scope=offline_access%20https%3A%2F%2Fwgmail.onmicrosoft.com%2Ff863619c-ea91-4f1d-85f4-2f907c53963b%2Fuser_impersonation%20https%3A%2F%2Fgraph.microsoft.com%2Fmail.read&state=%7B%22hash%22%3A%22%22%7D&domain_hint=wgmail.de");
+        // But I am afraid the client id might change at some point which would completely destroy the login
     }
 
     private static String extractRefreshTokenFromUrl(String url) {
