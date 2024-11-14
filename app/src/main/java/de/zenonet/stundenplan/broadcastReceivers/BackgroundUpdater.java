bcf0afb4cd63339e5d3ad79e65ae9e6141039d28
zenonet <78077158+zenonet@users.noreply.github.com>
2024-11-14 15:56:29 +0100

    Made status notifications group similar lessons

--- app/src/main/java/de/zenonet/stundenplan/broadcastReceivers/BackgroundUpdater.java
+++ app/src/main/java/de/zenonet/stundenplan/broadcastReceivers/BackgroundUpdater.java
@@ -1,11 +1,9 @@
 package de.zenonet.stundenplan.broadcastReceivers;
 
 import android.Manifest;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
@@ -14,16 +12,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.preference.PreferenceManager;
 
-import com.google.gson.Gson;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Locale;
-import java.util.Objects;
 
 import de.zenonet.stundenplan.StundenplanPhoneApplication;
-import de.zenonet.stundenplan.activities.TimeTableViewActivity;
 import de.zenonet.stundenplan.common.DataNotAvailableException;
 import de.zenonet.stundenplan.common.Formatter;
 import de.zenonet.stundenplan.common.LogTags;
@@ -33,64 +24,12 @@ import de.zenonet.stundenplan.common.timetableManagement.Lesson;
 import de.zenonet.stundenplan.common.timetableManagement.TimeTable;
 import de.zenonet.stundenplan.common.timetableManagement.TimeTableManager;
 import de.zenonet.stundenplan.common.timetableManagement.UserLoadException;
-import de.zenonet.stundenplan.glance.TimetableWidgetKt;
 
 public class BackgroundUpdater extends BroadcastReceiver {
 
-    private Context context;
-
-    // This is not tested
-    private void manageShortTermChanges(TimeTable timeTable) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        long lastCounter = preferences.getLong("shortTermChanges_lastCounter", -1);
-
-        if (lastCounter == -1 || timeTable.CounterValue <= lastCounter) return;
-
-        File cacheFile = new File(context.getCacheDir(), "/shortTermChangeWatcherCache.json");
-        try {
-
-            TimeTable oldTimeTable = new Gson().fromJson(new FileReader(cacheFile), TimeTable.class);
-            Lesson[] oldDay = oldTimeTable.Lessons[Timing.getCurrentDayOfWeek()];
-            Lesson[] day = timeTable.Lessons[Timing.getCurrentDayOfWeek()];
-
-            for (int i = 0; i < 8; i++) {
-                if (Objects.equals(oldDay[i], day[i])) continue;
-                Lesson l = day[i];
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-
-                PendingIntent onClickIntent = PendingIntent.getActivity(context, 0,
-                        new Intent(context, TimeTableViewActivity.class), PendingIntent.FLAG_IMMUTABLE);
-
-                // Show notification
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, StundenplanPhoneApplication.TIME_TABLE_UPDATE_CHANNEL_ID)
-                        .setSmallIcon(de.zenonet.stundenplan.common.R.mipmap.ic_launcher)
-                        .setContentTitle("Kurzfristige Stundenplan-Änderung!")
-                        .setContentIntent(onClickIntent)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH);
-
-                // Check if the lesson is cancelled
-                if ((oldDay[i] != null && oldDay[i].isTakingPlace() && (l == null || !l.isTakingPlace())))
-                    builder.setContentText(String.format(Locale.GERMANY, "Heute %d. Stunde: Entfall", i + 1));
-                    // Check if the new lesson is an extra lesson
-                else if ((oldDay[i] == null || !oldDay[i].isTakingPlace() && (l != null && l.isTakingPlace()))) {
-                    builder.setContentText(String.format(Locale.GERMANY, "Heute %d. Stunde: Zusatzstunde %s in %s mit %s", i + 1, l.SubjectShortName, l.Room, l.Teacher));
-                } else {
-                    builder.setContentText(String.format(Locale.GERMANY, "Heute %d. Stunde: %s in %s mit %s", i + 1, l.SubjectShortName, l.Room, l.Teacher));
-                }
-
-                notificationManager.notify(444 + i, builder.build());
-            }
-
-            preferences.edit().putLong("shortTermChanges_lastHashCode", timeTable.CounterValue).apply();
-            Utils.writeAllText(cacheFile, new Gson().toJson(timeTable));
-        } catch (IOException | SecurityException e) {
-            Log.e(LogTags.BackgroundWork, e.getMessage());
-        }
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        this.context = context;
         Log.i(LogTags.BackgroundWork, "Running background updater...");
         int dayOfWeek = Timing.getCurrentDayOfWeek();
 
@@ -126,9 +65,6 @@ public class BackgroundUpdater extends BroadcastReceiver {
                     return;
                 }
 
-                //manageShortTermChanges(timeTable);
-                //int dayOfWeek = 0;
-
                 if (!PreferenceManager.getDefaultSharedPreferences(context).getBoolean("showNotifications", false))
                     return;
 
@@ -139,7 +75,6 @@ public class BackgroundUpdater extends BroadcastReceiver {
                 }
 
                 Lesson nextLesson = timeTable.Lessons[dayOfWeek][nextPeriod];
-                Lesson lessonAfterThat = timeTable.Lessons[dayOfWeek].length > nextPeriod + 1 ? timeTable.Lessons[dayOfWeek][nextPeriod + 1] : null;
 
 
                 // Just don't show a notification if the next lesson is not taking place
@@ -147,22 +82,35 @@ public class BackgroundUpdater extends BroadcastReceiver {
                     notificationManager.cancel(666);
                     return; // TODO: Make it create a notification here, that says when the next lesson starts
                 }
+                int count = 1;
+                for(int i = nextPeriod+1; i < timeTable.Lessons[dayOfWeek].length; i++){
+                    Lesson lesson = timeTable.Lessons[dayOfWeek][i];
+                    if(lesson == null || lesson.Type != nextLesson.Type || !lesson.SubjectShortName.equals(nextLesson.SubjectShortName) || !lesson.Room.equals(nextLesson.Room))
+                        break;
+                    count++;
+                }
                 Formatter formatter = new Formatter(context);
+                String title = String.format("%s in %s mit %s bis %s",
+                            nextLesson.Subject,
+                            formatter.formatRoomName(nextLesson.Room),
+                            formatter.formatTeacherName(nextLesson.Teacher),
+                            nextLesson.EndTime);
+                if(count > 1){
+                    title = String.format(Locale.GERMANY, "%dx %s", count, title);
+                }
+
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(context, StundenplanPhoneApplication.STATUS_CHANNEL_ID)
-                        .setSmallIcon(de.zenonet.stundenplan.common.R.mipmap.ic_launcher)
-                        .setContentTitle(String.format("%s %s mit %s bis %s",
-                                formatter.formatRoomName(nextLesson.Room),
-                                nextLesson.Subject,
-                                formatter.formatTeacherName(nextLesson.Teacher),
-                                nextLesson.EndTime))
                         .setSmallIcon(de.zenonet.stundenplan.common.R.drawable.ic_notification_icon)
+                        .setContentTitle(title)
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                         .setSilent(true);
 
+                Lesson lessonAfterThat = timeTable.Lessons[dayOfWeek].length > nextPeriod + count ? timeTable.Lessons[dayOfWeek][nextPeriod + count] : null;
+
                 if (lessonAfterThat != null && lessonAfterThat.isTakingPlace()) {
-                    builder.setContentText(String.format("Danach: %s %s mit %s",
-                            formatter.formatRoomName(lessonAfterThat.Room),
+                    builder.setContentText(String.format("Danach: %s in %s mit %s",
                             lessonAfterThat.Subject,
+                            formatter.formatRoomName(lessonAfterThat.Room),
                             formatter.formatTeacherName(lessonAfterThat.Teacher)
                     ));
                 }
