dfb540d80168bea00f91e173bb479b8fff43160e
zenonet <78077158+zenonet@users.noreply.github.com>
2024-12-01 01:00:16 +0100

    Fully migrated TimeTableApiClient to OkHttp

--- Common/src/main/java/de/zenonet/stundenplan/common/timetableManagement/TimeTableApiClient.java
+++ Common/src/main/java/de/zenonet/stundenplan/common/timetableManagement/TimeTableApiClient.java
@@ -15,23 +15,19 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 import de.zenonet.stundenplan.common.DataNotAvailableException;
 import de.zenonet.stundenplan.common.ResultType;
 import de.zenonet.stundenplan.common.LogTags;
 import de.zenonet.stundenplan.common.NameLookup;
-import de.zenonet.stundenplan.common.Utils;
 import de.zenonet.stundenplan.common.callbacks.AuthCodeRedeemedCallback;
 import de.zenonet.stundenplan.common.models.User;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class TimeTableApiClient {
@@ -55,15 +51,12 @@ public class TimeTableApiClient {
      */
     public boolean fetchMasterData() throws DataNotAvailableException {
 
-        try {
+        Request request = getAuthenticatedRequestBuilder("all").build();
+        try(Response response = client.newCall(request).execute()) {
             // Get raw json data
-            HttpURLConnection httpCon = getAuthenticatedUrlConnection("GET", "/all");
-            httpCon.connect();
-            int respCode = httpCon.getResponseCode();
-            if (respCode != 200)
+            if (!response.isSuccessful())
                 throw new DataNotAvailableException();
-            String raw = Utils.readAllFromStream(httpCon.getInputStream());
-            return lookup.saveLookupFile(raw);
+            return lookup.saveLookupFile(response.body().string());
         } catch (Exception e) {
             throw new DataNotAvailableException();
         }
@@ -75,16 +68,13 @@ public class TimeTableApiClient {
 
 
     public User getUser() throws UserLoadException {
-        try {
-            HttpURLConnection httpCon = getAuthenticatedUrlConnection("GET", "me");
-            httpCon.connect();
+        Request request = getAuthenticatedRequestBuilder("me").build();
+        try(Response response = client.newCall(request).execute()) {
 
-            int respCode = httpCon.getResponseCode();
-            if (respCode != 200) {
-                Log.e(LogTags.Api, String.format("Unable to load personal data: Response code %d", respCode));
+            if (response.isSuccessful()) {
+                Log.e(LogTags.Api, String.format("Unable to load personal data: Response code %d", response.code()));
             }
-
-            user = new Gson().fromJson(new InputStreamReader(httpCon.getInputStream()), User.class);
+            user = new Gson().fromJson(new InputStreamReader(response.body().byteStream()), User.class);
             return user;
         } catch (Exception e) {
             throw new UserLoadException();
@@ -95,33 +85,32 @@ public class TimeTableApiClient {
         String refreshToken = sharedPreferences.getString("refreshToken", "null");
 
         if (refreshToken.equals("null")) return ResultType.NoLoginSaved;
-        try {
-            HttpURLConnection httpCon = getUrlApiConnection("POST", "token");
-            httpCon.setDoOutput(true);
 
-            OutputStream os = httpCon.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
-            osw.write("{\"refresh_token\":\"" + refreshToken + "\",\"scope\":\"https://wgmail.onmicrosoft.com/f863619c-ea91-4f1d-85f4-2f907c53963b/user_impersonation\"}");
-            osw.flush();
-            osw.close();
-            os.close();
+        Request request = new Request.Builder()
+                .url(baseUrl + "token")
+                .post(RequestBody.create(
+                        "{\"refresh_token\":\"" + refreshToken + "\",\"scope\":\"https://wgmail.onmicrosoft.com/f863619c-ea91-4f1d-85f4-2f907c53963b/user_impersonation\"}",
+                        MediaType.get("application/json")
+                ))
+                .build();
 
-            int respCode = httpCon.getResponseCode();
+        try(Response response = client.newCall(request).execute()) {
+            int respCode = response.code();
 
             if (respCode != 200) {
-                Log.e(LogTags.Api, "Login unsuccessful:" + httpCon.getResponseMessage());
+                Log.e(LogTags.Api, "Login unsuccessful:" + response.message());
                 return ResultType.TokenExpired;
             }
 
-            String content = Utils.readAllFromStream(httpCon.getInputStream());
-
             // Deserialization
-            JSONObject jObj = new JSONObject(content);
+            JSONObject jObj = new JSONObject(response.body().string());
 
             sharedPreferences.edit().putString("refreshToken", jObj.getString("refresh_token")).apply();
             accessToken = jObj.getString("access_token");
+
             isLoggedIn = true;
             Log.i(LogTags.Login, "Logged in successfully");
+
             return ResultType.Success;
         } catch (IOException | JSONException e) {
             if(e instanceof UnknownHostException) {
@@ -136,43 +125,39 @@ public class TimeTableApiClient {
 
     public void redeemOAuthCodeAsync(String code, AuthCodeRedeemedCallback callback){
         new Thread(() -> {
-            HttpURLConnection httpCon;
-            try {
-                URL url = new URL("https://www.wolkenberg-gymnasium.de/wolkenberg-app/api/token");
-                httpCon = (HttpURLConnection) url.openConnection();
-                httpCon.setRequestMethod("POST");
-                httpCon.setRequestProperty("Content-Type", "application/json");
-                httpCon.setRequestProperty("referer", "https://www.wolkenberg-gymnasium.de/wolkenberg-app/stundenplan-web-app/");
-                httpCon.setDoOutput(true);
-                httpCon.setDoInput(true);
+            Request request = new Request.Builder()
+                    .url("https://www.wolkenberg-gymnasium.de/wolkenberg-app/api/token")
+                    .header("Content-Type", "application/json")
+                    .header("referer", "https://www.wolkenberg-gymnasium.de/wolkenberg-app/stundenplan-web-app/")
+                    .post(RequestBody.create(
+                            "{\"code\":\"" + code + "\",\"scope\":\"https://wgmail.onmicrosoft.com/f863619c-ea91-4f1d-85f4-2f907c53963b/user_impersonation\"}",
+                            MediaType.get("application/json"))
+                    )
+                    .build();
 
-                OutputStream os = httpCon.getOutputStream();
-                OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
-                osw.write("{\"code\":\"" + code + "\",\"scope\":\"https://wgmail.onmicrosoft.com/f863619c-ea91-4f1d-85f4-2f907c53963b/user_impersonation\"}");
-                osw.flush();
-                osw.close();
-                os.close();
-
-                int respCode = httpCon.getResponseCode();
+            try(Response response = client.newCall(request).execute()) {
+                int respCode = response.code();
                 Log.i(LogTags.Api, "Get response code " + respCode + " while redeeming OAuth code");
 
-                if(respCode != 200){
+                if (respCode != 200) {
                     callback.errorOccurred(String.format(Locale.GERMAN, "Der Server hat mit Antwort-Code %d geantwortet", respCode));
                     return;
                 }
 
-                String body = Utils.readAllFromStream(httpCon.getInputStream());
-                JSONObject jObj = new JSONObject(body);
+                // get access token
+                JSONObject jObj = new JSONObject(response.body().string());
                 accessToken = jObj.getString("access_token");
-                // isLoggedIn = true; This would stop the login method from doing anything meaning the masterdata wouldn't be fetched
 
+                // get refresh token
                 String refreshToken = jObj.getString("refresh_token");
                 sharedPreferences.edit().putString("refreshToken", refreshToken).apply();
 
                 if (callback != null) callback.authCodeRedeemed();
 
-            } catch (IOException | JSONException e) {
-                callback.errorOccurred(e.getMessage());
+
+            } catch (IOException | JSONException | AssertionError e) {
+                if(callback != null)
+                    callback.errorOccurred(e.getMessage());
             }
         }).start();
     }
@@ -227,35 +212,19 @@ public class TimeTableApiClient {
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + accessToken);
     }
-    public HttpURLConnection getAuthenticatedUrlConnection(String method, String endpoint) throws IOException {
-        HttpURLConnection httpCon = getUrlApiConnection(method, endpoint);
-        httpCon.setRequestProperty("Authorization", "Bearer " + accessToken);
-        return httpCon;
-    }
-
-    public HttpURLConnection getUrlApiConnection(String method, String endpoint) throws IOException {
-        HttpURLConnection httpCon = (HttpURLConnection) new URL(baseUrl + endpoint).openConnection();
-        httpCon.setRequestMethod(method);
-        httpCon.setRequestProperty("Content-Type", "application/json");
-        httpCon.setRequestProperty("connection", "close");
-        return httpCon;
-    }
-
     public String getRawData() throws IOException {
-        HttpURLConnection httpCon = getAuthenticatedUrlConnection("GET", "timetable/student/" + user.id);
-        httpCon.connect();
-        if (httpCon.getResponseCode() != 200)
-            throw new IOException();
-
-        return Utils.readAllFromStream(httpCon.getInputStream());
+        Request request = getAuthenticatedRequestBuilder("timetable/student/" + user.id).build();
+        try(Response response = client.newCall(request).execute()){
+            if(!response.isSuccessful()) throw new IOException("Request for timetable/student/ not successful");
+            return response.body().string();
+        }
     }
     public String getRawSubstitutionData() throws IOException {
-        HttpURLConnection httpCon = getAuthenticatedUrlConnection("GET", "substitution/student/" + user.id);
-        httpCon.connect();
-        if (httpCon.getResponseCode() != 200)
-            throw new IOException();
-
-        return Utils.readAllFromStream(httpCon.getInputStream());
+        Request request = getAuthenticatedRequestBuilder("substitution/student/" + user.id).build();
+        try(Response response = client.newCall(request).execute()){
+            if(!response.isSuccessful()) throw new IOException("Request for timetable/student/ not successful");
+            return response.body().string();
+        }
     }
 
     /**
@@ -294,13 +263,12 @@ public class TimeTableApiClient {
     }
 
     public Post[] getPosts(){
-        try {
-            HttpURLConnection connection = getAuthenticatedUrlConnection("GET", "posts/");
-            connection.connect();
-            if (connection.getResponseCode() != 200)
+        Request request = getAuthenticatedRequestBuilder("posts").build();
+        try(Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful())
                 return null;
-            String rawJson = Utils.readAllFromStream(connection.getInputStream());
-            JSONArray array = new JSONArray(rawJson);
+
+            JSONArray array = new JSONArray(response.body().string());
             Post[] posts = new Post[array.length()];
 
             for (int i = 0; i < array.length(); i++) {
@@ -331,7 +299,8 @@ public class TimeTableApiClient {
 
             return posts;
         } catch (IOException | JSONException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            return null;
         }
     }
 }
