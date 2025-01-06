30377e7b6febe7d119728cf054ddf101d147bc46
zenonet <78077158+zenonet@users.noreply.github.com>
2025-01-06 20:41:05 +0100

    Fixed misrendering copy icon in TokenViewerActivity by making use of a drawable ressource

--- app/src/main/java/de/zenonet/stundenplan/activities/TokenViewerActivity.kt
+++ app/src/main/java/de/zenonet/stundenplan/activities/TokenViewerActivity.kt
@@ -26,15 +26,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathFillType
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -42,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.preference.PreferenceManager
 import de.zenonet.stundenplan.ExpandableCard
+import de.zenonet.stundenplan.R
 import de.zenonet.stundenplan.TokenViewerViewModel
 import de.zenonet.stundenplan.common.timetableManagement.TimeTableManager
 import de.zenonet.stundenplan.homework.ViewModelFactory
@@ -157,66 +152,9 @@ fun TokenView(token: String, modifier: Modifier = Modifier) {
         Toast.makeText(localContext, "In Zwischenablage kopiert", Toast.LENGTH_SHORT).show()
         clipboardManager.setText(AnnotatedString(token))
     }) {
-        Icon(Content_copy, "copy token")
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_content_copy_24),
+            contentDescription = "copy token"
+        )
     }
 }
-
-public val Content_copy: ImageVector
-    get() {
-        if (_Content_copy != null) {
-            return _Content_copy!!
-        }
-        _Content_copy = ImageVector.Builder(
-            name = "Content_copy",
-            defaultWidth = 24.dp,
-            defaultHeight = 24.dp,
-            viewportWidth = 960f,
-            viewportHeight = 960f
-        ).apply {
-            path(
-                fill = SolidColor(Color.Black),
-                fillAlpha = 1.0f,
-                stroke = null,
-                strokeAlpha = 1.0f,
-                strokeLineWidth = 1.0f,
-                strokeLineCap = StrokeCap.Butt,
-                strokeLineJoin = StrokeJoin.Miter,
-                strokeLineMiter = 1.0f,
-                pathFillType = PathFillType.NonZero
-            ) {
-                moveTo(360f, 720f)
-                quadToRelative(-33f, 0f, -56.5f, -23.5f)
-                reflectiveQuadTo(280f, 640f)
-                verticalLineToRelative(-480f)
-                quadToRelative(0f, -33f, 23.5f, -56.5f)
-                reflectiveQuadTo(360f, 80f)
-                horizontalLineToRelative(360f)
-                quadToRelative(33f, 0f, 56.5f, 23.5f)
-                reflectiveQuadTo(800f, 160f)
-                verticalLineToRelative(480f)
-                quadToRelative(0f, 33f, -23.5f, 56.5f)
-                reflectiveQuadTo(720f, 720f)
-                close()
-                moveToRelative(0f, -80f)
-                horizontalLineToRelative(360f)
-                verticalLineToRelative(-480f)
-                horizontalLineTo(360f)
-                close()
-                moveTo(200f, 880f)
-                quadToRelative(-33f, 0f, -56.5f, -23.5f)
-                reflectiveQuadTo(120f, 800f)
-                verticalLineToRelative(-560f)
-                horizontalLineToRelative(80f)
-                verticalLineToRelative(560f)
-                horizontalLineToRelative(440f)
-                verticalLineToRelative(80f)
-                close()
-                moveToRelative(160f, -240f)
-                verticalLineToRelative(-480f)
-                close()
-            }
-        }.build()
-        return _Content_copy!!
-    }
-
-private var _Content_copy: ImageVector? = null
