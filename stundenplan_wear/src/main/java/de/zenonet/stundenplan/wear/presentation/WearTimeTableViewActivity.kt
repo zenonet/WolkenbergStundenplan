zenonet/WolkenbergStundenplan

commit facc9cec90e5ded1dee89e79844e08fc25ee8061

Made wearOs app differentiate between holiday and loading error

stundenplan_wear/src/main/java/de/zenonet/stundenplan/wear/presentation/WearTimeTableViewActivity.kt
@@ -179,9 +179,7 @@ fun TimeTable(viewModel: WearTimeTableViewModel) {
                         state = listState,
                     ) {
 
-                        if (timeTable != null && !timeTable!!.Lessons[day].isEmpty()) {
-
-
+                        if (timeTable != null && !timeTable!!.Lessons[day].isEmpty() && timeTable!!.Lessons[day].none { it == null || !it.isTakingPlace }) {
                             val currentPeriod = Utils.getCurrentPeriod(Timing.getCurrentTime())
                             //items(viewModel.timeTable.value!!.Lessons[day].size) { period ->
                             items(8) { period ->
@@ -213,7 +211,14 @@ fun TimeTable(viewModel: WearTimeTableViewModel) {
                                     }
                                 }
                             }
-                        } else {
+                        } else if(timeTable == null){
+                            item{
+                                Text("Fehler",
+                                    color = colorResource(id = CommonR.color.substituted_lesson),
+                                    fontSize = 20.sp
+                                )
+                            }
+                        }else{
                             item {
                                 Text("Frei",
                                     color = colorResource(id = CommonR.color.cancelled_lesson),

stundenplan_wear/src/main/java/de/zenonet/stundenplan/wear/presentation/WearTimeTableViewModel.kt
@@ -82,11 +82,13 @@ class WearTimeTableViewModel(val startLoginActivity: () -> Unit) : ViewModel() {
             }ms"
         )
 
+        var gotData = false
         timeTableManager!!.getTimeTableAsyncWithAdjustments(selectedWeek, { timeTable ->
             if(timeTable == null) {
                 Log.i(LogTags.Debug, "Got null timetable")
                 return@getTimeTableAsyncWithAdjustments
             }
+            gotData = true
             Log.i(
                 LogTags.Timing,
                 "Got timetable from ${timeTable.source}${if (timeTable.isCacheStateConfirmed) " (confirmed)" else ""} (${
@@ -100,9 +102,11 @@ class WearTimeTableViewModel(val startLoginActivity: () -> Unit) : ViewModel() {
             _timeTable.postValue(timeTable)
             isLoading = false
         },{ error ->
-            isLoading = false
-            timeTableDirect = null
-            _timeTable.postValue(null)
+            if(!gotData) {
+                isLoading = false
+                timeTableDirect = null
+                _timeTable.postValue(null)
+            }
         })
     }
 

commit 15e664ad7f013d7e5aee5180dea3a67efffbbdc4

Fixed bug where days would be shown if they had any free lessons on wearOS(introduced in facc9cec)

stundenplan_wear/src/main/java/de/zenonet/stundenplan/wear/presentation/WearTimeTableViewActivity.kt
@@ -179,7 +179,7 @@ fun TimeTable(viewModel: WearTimeTableViewModel) {
                         state = listState,
                     ) {
 
-                        if (timeTable != null && !timeTable!!.Lessons[day].isEmpty() && timeTable!!.Lessons[day].none { it == null || !it.isTakingPlace }) {
+                        if (timeTable != null && !timeTable!!.Lessons[day].isEmpty() && !timeTable!!.Lessons[day].all { it == null || !it.isTakingPlace }) {
                             val currentPeriod = Utils.getCurrentPeriod(Timing.getCurrentTime())
                             //items(viewModel.timeTable.value!!.Lessons[day].size) { period ->
                             items(8) { period ->