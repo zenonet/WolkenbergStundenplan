9a20bf233b85b7121e5940d5a2f0dcc3a02ef31f
zenonet <git@zenonet.de>
2025-08-23 01:01:38 +0200

    Updated dependencies

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -34,14 +34,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.preference:preference:1.2.1")
 
     // OkHttp
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:5.1.0")
 
 
     testImplementation("junit:junit:4.13.2")
