02c287cfc8cbedfe65d7c82100a9f3e0eb40fac6
zenonet <git@zenonet.de>
2025-09-06 14:11:03 +0200

    Updated dependencies

--- stundenplan_wear/build.gradle.kts
+++ stundenplan_wear/build.gradle.kts
@@ -54,16 +54,16 @@ android {
 
 dependencies {
 
-    implementation("com.google.android.gms:play-services-wearable:18.2.0")
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("com.google.android.gms:play-services-wearable:19.0.0")
+    implementation(platform("androidx.compose:compose-bom:2025.08.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.wear.compose:compose-material:1.3.1")
-    implementation("androidx.wear.compose:compose-foundation:1.3.1")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.wear.compose:compose-material:1.5.0")
+    implementation("androidx.wear.compose:compose-foundation:1.5.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.wear:wear-remote-interactions:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.wear:wear-remote-interactions:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.wear:wear-complications-data-source-ktx:1.0.0-alpha22")
@@ -72,7 +72,7 @@ dependencies {
     //androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.3")
 
 /*
     implementation("androidx.compose.runtime:runtime-tracing:1.0.0-beta01")
